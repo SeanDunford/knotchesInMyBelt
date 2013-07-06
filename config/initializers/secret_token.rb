3f74e8040e43ac4c255ae84e42931ce82f6271de
Sean Dunford <sdunford@mail.usf.edu>
2013-07-06 01:16:32 -0700

    push it

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KnotchesInMyBelt::Application.config.secret_token = '49240327ee18da1a84dccad23ad914d936080ea3209dde094d0ed1f6d13c3da8b4a205ffa9bf23a704be3e157a157e314d91446024362d74474a7c317eef481a'
