f876041330315dc7bd8754958fbfb3cd7dcfe7d7
Sean Dunford <sdunford@mail.usf.edu>
2013-07-09 01:50:46 -0700

    got a lot done by switching to HandleBars just need to figure out how to merge my comment generation code with my knotchgeneration code and I should be good. Minus a few small UI changes

--- knotchMiniProject/sourceCode/javascript/getKnotchData.js
+++ knotchMiniProject/sourceCode/javascript/getKnotchData.js
@@ -1,7 +1,7 @@
 function doStuff()
 	{ 
 		$.ajax({
-		url: "http://127.0.0.1:8080/?proxy="+encodeURIComponent("http://dev.knotch.it:8080/miniProject/user_feed/500e3b57bbcd08696800000a/10"),
+		url: "http://127.0.0.1:8080/?proxy="+encodeURIComponent("http://dev.knotch.it:8080/miniProject/user_feed/500e3b57bbcd08696800000a/1000"),
 		context: document.body
 	}).done(function(data) {
 		console.log(data);
@@ -18,30 +18,11 @@ function doStuff()
 			"18":	["#ff6d3a"],	
 			"20":	["#ee443a"]	
 			};
-		//$('#headerName').text(data.userInfo.name); 
-		//$("#userName").text(data.userInfo.name); 
-		$("#userLocation").text(data.userInfo.location);
-		$("#TopicStatsNumber").text(data.userInfo.num_topics); 
-		$("#FollowersStatsNumber").text(data.userInfo.num_followers); 
-		$("#FollowingStatsNumber").text(data.userInfo.num_following); 
-		$("#GloryStatsNumber").text(data.userInfo.num_glory); 
-		var listOfImgTags = $('.userImage')
-		for(i=0;i<listOfImgTags.length;i++)
-			{ 
-				$(listOfImgTags[i]).attr("src", data.userInfo.profilePicUrl);
-			}
-		var listOfNameTags = $('.userName')
-		for(i=0;i<listOfNameTags.length;i++)
-			{ 
-				$(listOfNameTags[i]).text(data.userInfo.name)
-			}
+
 
 		if(data.knotches.length > 0){
-			var commentToAppend, knotchToAppend =""; 
 			for(var knotchCount=0;knotchCount < data.knotches.length;knotchCount++)
 			{	
-				$(".knotchHeaderTitle").text(data.knotches[knotchCount].topic); 
-				$(".usersOpinion").text(data.knotches[knotchCount].comment);
 				$(".usersOpinionBubble").css("background-color", colorCode[data.knotches[knotchCount].sentiment]); 		
 				if(data.knotches[knotchCount].sentiment == "10"){
 					$(".usersOpinionBubble").css("border","1px solid #000000"); 
@@ -67,22 +48,39 @@ function otherStuff(){
 		context: document.body
 	}).done(function(data){
 		var knotches = data.knotches; 
-		console.log(knotches);
+		$("#userLocation").text(data.userInfo.location);
+		$("#TopicStatsNumber").text(data.userInfo.num_topics); 
+		$("#FollowersStatsNumber").text(data.userInfo.num_followers); 
+		$("#FollowingStatsNumber").text(data.userInfo.num_following); 
+		$("#GloryStatsNumber").text(data.userInfo.num_glory); 
+		var listOfImgTags = $('.userImage')
+		for(i=0;i<listOfImgTags.length;i++)
+			{ 
+				$(listOfImgTags[i]).attr("src", data.userInfo.profilePicUrl);
+			}
+		var listOfNameTags = $('.userName')
+		for(i=0;i<listOfNameTags.length;i++)
+			{ 
+				$(listOfNameTags[i]).text(data.userInfo.name)
+			}
 		if(knotches.length > 0){
 			var knotchContainer = $(".knotchContainer");
 			for(var knotchCounter = 0; knotchCounter < knotches.length; knotchCounter++){
-				//knotches[knotchCounter].topic
-				knotchContainer.append("<div class=\"knotches\"><\/div><div class=\"knotchHeader\"><div class=\"knotchHeaderSpacer\">&nbsp;<\/div><div class=\"knotchHeaderTitle\">"+knotches[knotchCounter].topic+"<\/div>");
-				knotchContainer.append("<div class=\"knotchHeaderArrow\"><a href=\"url\"><img src = \"assets\\topic-arrow.png\" alt=\"goToTopic\"><\/a><\/div><\/div>")
-				knotchContainer.append("<div class=\"usersOpinionSpacer\">&nbsp;<\/div><div class = \"usersOpinionBubble\"><div class=\"usersOpinion\">"+knotches[knotchCounter].comment+"<\/div><\/div><div class=\"clear-left\"><\/div>")
-				knotchContainer.append("<div class=\"knotchBoxSpacer\">&nbsp;<\/div><div class=\"knotchBox\"><img class =\"knotchTriangle\" src = \"knotchBoxes\\triangle0.png\" alt=\"goToKnotch\"><\/div>");
-				knotchContainer.append("<div class=\"smallSpacer\"><\/div><div class=\"usersOpinionSpacer\">&nbsp;<\/div><div class=\"knotchOwner\"><img width=\"42px\" class=\"userImage\" src=\"assets\/userPicture.gif\"><span class =\"UserName\">Aron Tzimas<\/span><\/div><div class=\"smallSpacer\"><\/div>");
-				knotchContainer.append("<div class=\"usersOpinionSpacer\">&nbsp;<\/div><div class=\"comment\"><input type=\"text\"  class =\"newCommentBox\" value=\"Comment on this knotch...\"><\/div><div class=\"clear-left\"><\/div><div class=\"smallSpacer\"><\/div>");
-				knotchContainer.append("");
-			}
+				
+				$(".usersOpinionBubble").css("background-color", colorCode[data.knotches[knotchCount].sentiment]); 		
+				if(data.knotches[knotchCount].sentiment == "10"){
+					$(".usersOpinionBubble").css("border","1px solid #000000"); 
+				}
+
+				knotchContainer.append(
+				generateKnotch(knotches[knotchCounter].topic, 
+							   knotches[knotchCounter].comment, 
+							   knotches[knotchCounter].userId.profilePicUrl, 
+							   knotches[knotchCounter].userId.name)); 
+		}
 		}
 	});
-	}
+}
 function handleBarTesting(){
 	var source   = $("#handleBarsTest").html();
 	var template = Handlebars.compile(source);
@@ -90,11 +88,18 @@ function handleBarTesting(){
 	var html    = template(context);
 	return html; 
 }
-
 function generateKnotchComment(pictureUrl, name, reply){
+	pictureUrl = "http://127.0.0.1:8080/?proxy="+ pictureUrl; 
 	var source   = $("#knotchComment").html();
 	var template = Handlebars.compile(source);
 	var context = {commentorsPicture: pictureUrl, commentorsName:name , commentorsReply: reply};
+	var html    = template(context);
+	return html; 
+}
+function generateKnotch(title, opinion, pictureUrl, name){
+	var source   = $("#knotch").html();
+	var template = Handlebars.compile(source);
+	var context = {knotchTitle:title, knotchersOpinion:opinion, knotchersPicture:pictureUrl, knotchersName:name};
 	var html    = template(context);
 	return html; 
 }
