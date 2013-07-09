c7d1ce1d395ddcb08be269fe02e80801b29c094a
Sean Dunford <sdunford@mail.usf.edu>
2013-07-09 02:15:46 -0700

    making it purty

--- knotchMiniProject/sourceCode/javascript/getKnotchData.js
+++ knotchMiniProject/sourceCode/javascript/getKnotchData.js
@@ -1,8 +1,8 @@
 function doStuff()
 	{ 
 		$.ajax({
-		url: "http://127.0.0.1:8080/?proxy="+encodeURIComponent("http://dev.knotch.it:8080/miniProject/user_feed/500e3b57bbcd08696800000a/1000"),
-		context: document.body
+		url: "http://127.0.0.1:8080/?proxy="+encodeURIComponent("http://dev.knotch.it:8080/miniProject/user_feed/5019296f1f5dc55304003c58/1000"),
+		context: document.body																					 
 	}).done(function(data) {
 		console.log(data);
 		var colorCode = {
@@ -44,10 +44,24 @@ function doStuff()
 }
 function otherStuff(){
 		$.ajax({
-		url: "http://127.0.0.1:8080/?proxy="+encodeURIComponent("http://dev.knotch.it:8080/miniProject/user_feed/500e3b57bbcd08696800000a/10"),
+		url: "http://127.0.0.1:8080/?proxy="+encodeURIComponent("http://dev.knotch.it:8080/miniProject/user_feed/5019296f1f5dc55304003c58/1000"),
 		context: document.body
 	}).done(function(data){
+		console.log(data); 
 		var knotches = data.knotches; 
+		var colorCode = {
+			"0":	["#2e5ca6"],	
+			"2":	["#586db9"],
+			"4":	["#008fd0"],	
+			"6":	["#57cccc"],	
+			"8":	["#ceebee"],	
+			"10":	["#ffffff"],	
+			"12":	["#ffeec3"],	
+			"14":	["#ffcc43"],	
+			"16":	["#ffa02d"],	
+			"18":	["#ff6d3a"],	
+			"20":	["#ee443a"]	
+		};
 		$("#userLocation").text(data.userInfo.location);
 		$("#TopicStatsNumber").text(data.userInfo.num_topics); 
 		$("#FollowersStatsNumber").text(data.userInfo.num_followers); 
@@ -66,9 +80,8 @@ function otherStuff(){
 		if(knotches.length > 0){
 			var knotchContainer = $(".knotchContainer");
 			for(var knotchCounter = 0; knotchCounter < knotches.length; knotchCounter++){
-				
-				$(".usersOpinionBubble").css("background-color", colorCode[data.knotches[knotchCount].sentiment]); 		
-				if(data.knotches[knotchCount].sentiment == "10"){
+				$(".usersOpinionBubble").css("background-color", colorCode[data.knotches[knotchCounter].sentiment]); 		
+				if(data.knotches[knotchCounter].sentiment == "10"){
 					$(".usersOpinionBubble").css("border","1px solid #000000"); 
 				}
 
@@ -96,7 +109,7 @@ function generateKnotchComment(pictureUrl, name, reply){
 	var html    = template(context);
 	return html; 
 }
-function generateKnotch(title, opinion, pictureUrl, name){
+function generateKnotch(title, opinion, pictureUrl, name, sentiment){
 	var source   = $("#knotch").html();
 	var template = Handlebars.compile(source);
 	var context = {knotchTitle:title, knotchersOpinion:opinion, knotchersPicture:pictureUrl, knotchersName:name};
