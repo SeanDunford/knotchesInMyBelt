113f3d0ea00db92eca88821a0b3fc79d37016562
Sean Dunford <sdunford@mail.usf.edu>
2013-07-09 20:41:00 -0700

    So Close

--- knotchMiniProject/sourceCode/javascript/getKnotchData.js
+++ knotchMiniProject/sourceCode/javascript/getKnotchData.js
@@ -1,67 +1,11 @@
-function doStuff()
-	{ 
-		$.ajax({
-		url: "http://127.0.0.1:8080/?proxy="+encodeURIComponent("http://dev.knotch.it:8080/miniProject/user_feed/5019296f1f5dc55304003c58/1000"),
-		context: document.body																					 
-	}).done(function(data) {
-		console.log(data);
-		var colorCode = {
-			"0":	["#2e5ca6"],	
-			"2":	["#586db9"],
-			"4":	["#008fd0"],	
-			"6":	["#57cccc"],	
-			"8":	["#ceebee"],	
-			"10":	["#ffffff"],	
-			"12":	["#ffeec3"],	
-			"14":	["#ffcc43"],	
-			"16":	["#ffa02d"],	
-			"18":	["#ff6d3a"],	
-			"20":	["#ee443a"]	
-			};
 
-
-		if(data.knotches.length > 0){
-			for(var knotchCount=0;knotchCount < data.knotches.length;knotchCount++)
-			{	
-				$(".usersOpinionBubble").css("background-color", colorCode[data.knotches[knotchCount].sentiment]); 		
-				if(data.knotches[knotchCount].sentiment == "10"){
-					$(".usersOpinionBubble").css("border","1px solid #000000"); 
-				}
-				 var triangle = "knotchBoxes/triangle" + data.knotches[knotchCount].sentiment + ".png";
-				 $(".knotchTriangle").attr("src", triangle);
-				//Don't forget. What if it's a white bubble? 
-				if(data.knotches[knotchCount].replies.length > 0){
-					for(var replyCount=0;replyCount < data.knotches[knotchCount].replies.length;replyCount++)
-						{ 
-							$(".commentsContainer").append(generateKnotchComment(data.knotches[knotchCount].replies[replyCount].userId.profilePicUrl,
-												  data.knotches[knotchCount].replies[replyCount].userId.name,
-												  data.knotches[knotchCount].replies[replyCount].reply));
-						}
-				}
-			}
-		}
-	});
-}
 function otherStuff(){
 		$.ajax({
-		url: "http://127.0.0.1:8080/?proxy="+encodeURIComponent("http://dev.knotch.it:8080/miniProject/user_feed/5019296f1f5dc55304003c58/1000"),
+		url: "http://127.0.0.1:8080/?proxy="+encodeURIComponent("http://dev.knotch.it:8080/miniProject/user_feed/5019296f1f5dc55304003c58/10"),
 		context: document.body
 	}).done(function(data){
 		console.log(data); 
 		var knotches = data.knotches; 
-		var colorCode = {
-			"0":	["#2e5ca6"],	
-			"2":	["#586db9"],
-			"4":	["#008fd0"],	
-			"6":	["#57cccc"],	
-			"8":	["#ceebee"],	
-			"10":	["#ffffff"],	
-			"12":	["#ffeec3"],	
-			"14":	["#ffcc43"],	
-			"16":	["#ffa02d"],	
-			"18":	["#ff6d3a"],	
-			"20":	["#ee443a"]	
-		};
 		$("#userLocation").text(data.userInfo.location);
 		$("#TopicStatsNumber").text(data.userInfo.num_topics); 
 		$("#FollowersStatsNumber").text(data.userInfo.num_followers); 
@@ -80,20 +24,52 @@ function otherStuff(){
 		if(knotches.length > 0){
 			var knotchContainer = $(".knotchContainer");
 			for(var knotchCounter = 0; knotchCounter < knotches.length; knotchCounter++){
-				$(".usersOpinionBubble").css("background-color", colorCode[data.knotches[knotchCounter].sentiment]); 		
-				if(data.knotches[knotchCounter].sentiment == "10"){
-					$(".usersOpinionBubble").css("border","1px solid #000000"); 
-				}
-
-				knotchContainer.append(
+				var thisKnotch = $("<div>"); 
+				thisKnotch.attr("class", "knotchNumber"+knotchCounter);
+				var reply = data.knotches[knotchCounter].replies;
+				var knotchComments; 
+				var sentimentColor = getSentimentColor(knotches[knotchCounter].sentiment);
+				thisKnotch.append(
 				generateKnotch(knotches[knotchCounter].topic, 
 							   knotches[knotchCounter].comment, 
 							   knotches[knotchCounter].userId.profilePicUrl, 
-							   knotches[knotchCounter].userId.name)); 
+							   knotches[knotchCounter].userId.name,
+							   sentimentColor,
+							   knotches[knotchCounter].sentiment)); 
+				if(reply.length > 0){
+					for (var replyCounter =0; replyCounter < reply.length; replyCounter++){
+						
+						knotchComments = generateKnotchComment(reply[replyCounter].userId.profilePicUrl,
+																   reply[replyCounter].userId.name,
+																   reply[replyCounter].reply);
+					}
+				}
+				thisKnotch.append(knotchComments); 
+				thisKnotch.append(generateNewCommentBox());
+				knotchContainer.append(thisKnotch);
 		}
 		}
 	});
 }
+function getSentimentColor(sentiment)
+{	
+	var colorCode = {
+	"0":	["#2e5ca6"],	
+	"2":	["#586db9"],
+	"4":	["#008fd0"],	
+	"6":	["#57cccc"],	
+	"8":	["#ceebee"],	
+	"10":	["#ffffff"],	
+	"12":	["#ffeec3"],	
+	"14":	["#ffcc43"],	
+	"16":	["#ffa02d"],	
+	"18":	["#ff6d3a"],	
+	"20":	["#ee443a"]	
+};		
+	//Don't forget. What if it's a white bubble?
+	return ("background-color: "+colorCode[sentiment]);
+}
+
 function handleBarTesting(){
 	var source   = $("#handleBarsTest").html();
 	var template = Handlebars.compile(source);
@@ -109,10 +85,18 @@ function generateKnotchComment(pictureUrl, name, reply){
 	var html    = template(context);
 	return html; 
 }
-function generateKnotch(title, opinion, pictureUrl, name, sentiment){
+function generateKnotch(title, opinion, pictureUrl, name, sentimentColor, sentiment){
 	var source   = $("#knotch").html();
 	var template = Handlebars.compile(source);
-	var context = {knotchTitle:title, knotchersOpinion:opinion, knotchersPicture:pictureUrl, knotchersName:name};
+	var context = {knotchTitle:title, knotchersOpinion:opinion, knotchersPicture:pictureUrl, knotchersName:name, insertableStuff:sentimentColor, triangleCode:sentiment };
+	var html    = template(context);
+	return html; 
+}
+function generateNewCommentBox()
+{
+	var source   = $("#newCommentBox").html();
+	var template = Handlebars.compile(source);
+	var context = {};
 	var html    = template(context);
 	return html; 
 }
